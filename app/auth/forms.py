fadb8b4e818095040af5834cc3ecdaa854e4d80a
Dara Cooper <dlloyd132@gmail.com>
2022-07-31 17:03:51 -0400

    day3

---
+++ app/auth/forms.py
@@ -0,0 +1,12 @@
+from tokenize import String
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, EqualTo
+
+class UserCreationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
+
